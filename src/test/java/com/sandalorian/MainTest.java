9ae7d1aa2ed21af6b0d3a59a11f5dd6e1f9d7702
Sandeep Reehall <35725129+sirReeall@users.noreply.github.com>
2024-08-08 15:40:21 +0100

    test build failure

---
+++ src/test/java/com/sandalorian/MainTest.java
@@ -0,0 +1,12 @@
+package com.sandalorian;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+  @Test
+  void testMain() {
+    fail();
+  }
+}
